63e2e8d5022c3a7c3412446fd905a342b47b709b
liveinmegacity <33742639+liveinmegacity@users.noreply.github.com>
2018-09-21 16:02:56 +0700

    Add strings test

--- Array/Array.cpp
+++ Array/Array.cpp
@@ -7,11 +7,9 @@ std::string get_string(
     unsigned int _length
   )
 {
-  static const char alphanum[] = "0123456789"
-                                 "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                                 "abcdefghijklmnopqrstuvwxyz";
+  static const char alphanum[] = "abcdefghijklmnopqrstuvwxyz";
   std::string str;
-  for (unsigned int i = 0; i < _length - 1; ++i)
+  for (unsigned int i = 0; i < _length; ++i)
   {
     str.push_back(alphanum[rand() % (sizeof(alphanum) - 1)]);
   }
@@ -42,7 +40,7 @@ int main()
 
   std::random_device device;
   std::mt19937 engine(device());
-  std::uniform_int_distribution<unsigned int> distribution(0, 100);
+  std::uniform_int_distribution<unsigned int> distValue(0, 100);
   CArray<int> dataN;
 
   // New array
@@ -51,9 +49,9 @@ int main()
 
   // Adding elements
   std::cout << "*** Adding elements:" << std::endl;
-  for (unsigned int i = 0; i < 20; i++)
+  for (int i = 0; i < 20; i++)
   {
-    dataN.push_back(distribution(engine));
+    dataN.push_back(distValue(engine));
   }
   print_array(dataN);
 
@@ -73,11 +71,11 @@ int main()
 
   // Adding random elements to random positions
   std::cout << "*** Adding random element to random position:" << std::endl;
-  for (unsigned int i = 0; i < 10; i++)
+  for (int i = 0; i < 10; i++)
   {
-    std::uniform_int_distribution<unsigned int> d(0, dataN.size() - 1);
-    unsigned int index = d(engine);
-    int value = distribution(engine);
+    std::uniform_int_distribution<unsigned int> distIndex(0, dataN.size() - 1);
+    unsigned int index = distIndex(engine);
+    int value = distValue(engine);
     dataN.insert(index, value);
     std::cout << "Index: " << index << ", value: " << value << std::endl;
     print_array(dataN);
@@ -97,4 +95,49 @@ int main()
             << std::endl
             << "*** STRINGS TEST ***" << std::endl
             << std::endl;
+
+  std::uniform_int_distribution<unsigned int> distString(1, 10);
+  CArray<std::string> dataS;
+
+  // New array
+  std::cout << "*** Just created array:" << std::endl;
+  print_array(dataS);
+
+  // Adding elements
+  std::cout << "*** Adding elements:" << std::endl;
+  for (int i = 0; i < 15; i++)
+  {
+    dataS.push_back(get_string(distString(engine)));
+  }
+  print_array(dataS);
+
+  // Sorting array
+  std::cout << "*** Sorting:" << std::endl;
+  dataS.sort();
+  print_array(dataS);
+
+  // Removing if
+  std::cout << "*** Removing words containing any of 'abcde':" << std::endl;
+  size = dataS.size();
+  for (int i = size - 1; i >= 0; i--)
+  {
+    if (dataS[i].find_first_of("abcde") != std::string::npos)
+    {
+      dataS.erase(i);
+    }
+  }
+  print_array(dataS);
+
+  // Adding random elements to random positions
+  std::cout << "*** Adding random element to random position:" << std::endl;
+  for (int i = 0; i < 3; i++)
+  {
+    std::uniform_int_distribution<unsigned int> distIndex(0, dataS.size() - 1);
+    unsigned int index = distIndex(engine);
+    std::string value = get_string(distString(engine));
+    dataS.insert(index, value);
+    std::cout << "Index: " << index << ", value: " << value << std::endl;
+    print_array(dataS);
+  }
+  print_array(dataS);
 }
